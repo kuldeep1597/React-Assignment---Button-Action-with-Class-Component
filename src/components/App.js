fefc6e9c103e4a91ed9e559b093521f60ec84eb0
kuldeep <kuldeep15764@gmail.com>
2020-11-23 13:29:12 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,23 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state = {
+			buttonstate: false
+		};
+		this.change=this.change.bind(this);
+	};
+	change() {
+		this.setState({ buttonstate: true });
 	};
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				<button onClick={this.change} id="click">button</button>
+				{ this.buttonstate ? (
+					<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+					
+				): <p>click again</p>}
     		</div>
     	);
     }
